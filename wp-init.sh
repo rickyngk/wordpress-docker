bf0127fa9cb43f6a6b00df52212f01329bc84582
Duy Nguyen <ricky.ngk@gmail.com>
2019-10-08 09:58:21 +0700

    remove temp file

--- wp-init.sh
+++
@@ -1,8 +0,0 @@
-RUN sed -i "/WP_DEBUG.*/a define('FS_METHOD', 'direct');" /var/www/html/wp-config.php 
-RUN chown -R www-data:www-data /var/www/html/wp-content/themes
-RUN chown -R www-data:www-data /var/www/html/wp-content/plugins
-RUN mkdir -p /var/www/html/wp-content/uploads/
-RUN chown -R www-data:www-data /var/www/html/wp-content/uploads/
-# execute apache
-exec "apache2-foreground"
-
